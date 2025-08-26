567188c8b32fc7b23c1fb5f184d80d67139f1615
Paul Zupan <paul.zupan.13@gmail.com>
2025-08-25 23:01:52 -0700

    Keep the player on screen

--- include/space-game/player.hpp
+++ include/space-game/player.hpp
@@ -5,7 +5,7 @@
 // Okay, so this definitely does not belong in this file. However, the player is
 // the only thing that needs this info right now (besides main), so this is
 // where it is going.
-constexpr sf::Vector2u screen_size{1920, 1080};
+constexpr sf::Vector2u screen_size{1600, 900};
 
 class Player : public sf::Drawable {
 public:
@@ -19,4 +19,7 @@ private:
 
     sf::Texture m_texture;
     sf::Sprite m_sprite;
+
+    void collide_screen_top();
+    void collide_screen_bottom();
 };
--- src/player.cpp
+++ src/player.cpp
@@ -1,5 +1,11 @@
 #include "space-game/player.hpp"
 
+#include <iostream>
+
+static void print_vector(sf::Vector2f v) {
+    std::cout << '(' << v.x << ", " << v.y << ")\n";
+}
+
 Player::Player() : m_texture("assets/ship.png"), m_sprite(m_texture) {
     sf::Vector2u s = m_texture.getSize();
 
@@ -12,11 +18,30 @@ Player::Player() : m_texture("assets/ship.png"), m_sprite(m_texture) {
 void Player::update(float dt) {
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
         m_sprite.move({0.0f, -speed * dt});
+        collide_screen_top();
     } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
         m_sprite.move({0.0f, speed * dt});
+        collide_screen_bottom();
     }
 }
 
 void Player::draw(sf::RenderTarget &target, sf::RenderStates states) const {
     target.draw(m_sprite);
+}
+
+void Player::collide_screen_top() {
+    auto bounds = m_sprite.getGlobalBounds();
+    if (bounds.position.y < 0.0f) {
+        m_sprite.move({0.0f, -bounds.position.y});
+    }
+}
+
+void Player::collide_screen_bottom() {
+    auto bounds = m_sprite.getGlobalBounds();
+    if (bounds.position.y + bounds.size.y > screen_size.y) {
+        m_sprite.move({
+            0.0f,
+            screen_size.y - bounds.position.y - bounds.size.y,
+        });
+    }
 }
