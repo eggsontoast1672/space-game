237327d3603febc9aae85377b6be8fb1800cda63
Paul Zupan <paul.zupan.13@gmail.com>
2025-08-25 20:32:38 -0700

    Moments before disaster

--- src/main.c
+++ src/main.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_image.h>
+#include <SDL3_image/SDL_image.h>
+
+const int SCREEN_WIDTH = 1600;
+const int SCREEN_HEIGHT = SCREEN_WIDTH * 9 / 16;
+
+void draw_player(SDL_Renderer *renderer, SDL_Texture *texture) {
+    float width, height;
+    SDL_GetTextureSize(texture, &width, &height);
+
+    SDL_FRect rect = {0.0f, 0.0f, width, height};
+
+    SDL_RenderTextureRotated(renderer, texture, NULL, &rect, 90.0, NULL,
+                             SDL_FLIP_NONE);
+}
 
 int main(void) {
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window *window = SDL_CreateWindow("C++ Game", 1920, 1080, 0);
+    SDL_Window *window =
+        SDL_CreateWindow("C++ Game", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+    SDL_Texture *background =
+        IMG_LoadTexture(renderer, "assets/background.png");
+    SDL_SetTextureScaleMode(background, SDL_SCALEMODE_NEAREST);
 
-    IMG_LoadTexture();
+    SDL_Texture *player_texture = IMG_LoadTexture(renderer, "assets/ship.png");
+    if (player_texture == NULL) {
+        SDL_DestroyTexture(background);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_SetTextureScaleMode(player_texture, SDL_SCALEMODE_NEAREST);
 
     bool window_should_close = false;
     while (!window_should_close) {
@@ -21,9 +47,13 @@ int main(void) {
         }
 
         SDL_RenderClear(renderer);
+        SDL_RenderTexture(renderer, background, NULL, NULL);
+        draw_player(renderer, player_texture);
         SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyTexture(player_texture);
+    SDL_DestroyTexture(background);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
